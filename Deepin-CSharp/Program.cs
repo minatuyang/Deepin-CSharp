f03a11b0872544e4e1f9918ca04a6121fce1a6f7
明豪 杨 <yangminghao2019@outlook.com>
2021-08-21 13:23:45 +0800

    initial

---
+++ Deepin-CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deepin_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
